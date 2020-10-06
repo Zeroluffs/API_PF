cdfb38253c403f68ab3612f85816907c540464b0
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-10-06 11:09:29 -0500

    added the host and port

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 const { mongoose } = require("./database");
+require("dotenv").config({ path: "variables.env" });
 
 //settings
-app.set("port", process.env.PORT || 3000);
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(cors());
@@ -18,10 +20,7 @@ app.use("/api/answer", require("./server/routes/answer.routes"));
 app.use("/api/students", require("./server/routes/students.routes"));
 app.use("/api/users", require("./server/routes/users.routes"));
 
-
-
-
 //starting server
-app.listen(app.get("port"), () => {
-  console.log("server on port", app.get("port"));
+app.listen(port, host, () => {
+  console.log("Server is Working");
 });
