f5b808b03916d197009118e3b67a5b5e5b155798
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-11-12 18:35:36 -0500

    now returns numbers of try per level

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -43,7 +43,11 @@ studentCtrl.getBestOfStudent = async (req, res) => {
   const test2 = [];
   for (i = 0; i < student.history.length; i++) {
     const n = student.history;
-    if (n[i].level == req.params.level && n[i].score != null && n[i].time != null) {
+    if (
+      n[i].level == req.params.level &&
+      n[i].score != null &&
+      n[i].time != null
+    ) {
       test2.push(n[i]);
     }
   }
@@ -58,6 +62,33 @@ studentCtrl.getBestOfStudent = async (req, res) => {
   //console.log(final[0])
   const bestScore = final[0];
   res.send(final);
+};
+
+studentCtrl.getTryPerLevel = async (req, res) => {
+  const student = await Student.findById(req.params.id).populate("history");
+  const test2 = [];
+  for (i = 0; i < student.history.length; i++) {
+    const n = student.history;
+    if (
+      n[i].level == req.params.level &&
+      n[i].score != null &&
+      n[i].time != null
+    ) {
+      test2.push(n[i]);
+    }
+  }
+  //test2.sort((a, b) => (Number(a.score) < Number(b.score) ? 1 : Number(b.score) < Number(a.score) ? -1 : 0));
+
+  const compare = function (a, b) {
+    return parseInt(b.score) - parseInt(a.score);
+  };
+
+  const final = test2.sort(compare);
+  //console.log(test2.sort(compare))
+  //console.log(final[0])
+  const bestScore = final[0];
+  const n = length(final);
+  res.send(n);
 };
 
 module.exports = studentCtrl;
--- server/routes/students.routes.js
+++ server/routes/students.routes.js
@@ -6,5 +6,7 @@ const student = require("../controllers/student.controller");
 router.get("/", student.getAllStudents);
 router.get("/:id", student.getStudent);
 router.get("/:id/:level", student.getBestOfStudent);
+router.get("/:id/try/:level", student.getTryPerLevel);
+
 router.put("/", student.makePassword);
 module.exports = router;
