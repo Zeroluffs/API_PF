98a2a7e6f7d9e8f6173eada4a1f34b2097f40bfa
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-11-12 18:42:10 -0500

    fixes

--- server/controllers/student.controller.js
+++ server/controllers/student.controller.js
@@ -87,8 +87,8 @@ studentCtrl.getTryPerLevel = async (req, res) => {
   //console.log(test2.sort(compare))
   //console.log(final[0])
   const bestScore = final[0];
-  const n = length(final);
-  res.send(n);
+  const n = final.length;
+  res.json({ tried: n });
 };
 
 module.exports = studentCtrl;
