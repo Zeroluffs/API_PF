7a676f716d780b914968c18b1a2b7309f066fbf3
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-12-06 15:25:10 -0500

    fixed some things

--- server/controllers/professor.controler.js
+++ server/controllers/professor.controler.js
@@ -56,7 +56,8 @@ professorCtrl.addStudent = async (req, res) => {
 
 professorCtrl.getStudents = async (req, res) => {
   const student = await Professor.findById(req.params.id).populate("student");
-  res.json(student.student);
+  const students = student.student;
+  res.json(students);
 };
 professorCtrl.logIn = async (req, res) => {
   const { email, password } = req.body;
