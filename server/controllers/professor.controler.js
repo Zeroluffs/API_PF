08be7f5e6c86fad5e2d32e4ec26e85f8894839a0
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-12-08 12:52:30 -0500

    .

--- server/controllers/professor.controler.js
+++ server/controllers/professor.controler.js
@@ -75,10 +75,7 @@ professorCtrl.logIn = async (req, res) => {
 
   //validates  hashed password in database with the one you just sent
   console.log("user data" + "" + user);
-  res.json({
-    _id: user._id,
-    role: user.role,
-  });
+  res.send(user);
 };
 
 professorCtrl.deleteStudent = async (req, res) => {
